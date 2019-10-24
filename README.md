019e7c33f2d9690e1d9dbc9b03acc63cce2defb4
wkimdev <wkimdev@gmail.com>
2019-10-24 23:55:38 +0900

    api gateway and websocket

--- README.md
+++ README.md
@@ -1,5 +1,6 @@
 # hello-serverless
 - serverless start
-  
+- websocket-chat
+
 #### content
 - [step1. 서버리스 시작하기](https://github.com/wkimdev/TIL/blob/master/AWS/AWS-Serverless-tutorials.md) 
--- handler.js
+++ handler.js
@@ -1,18 +1,113 @@
 'use strict';
+const AWS = require('aws-sdk');
+let dynamo = new AWS.DynamoDB.DocumentClient();
 
-module.exports.hello = async event => {
-  return {
+require('aws-sdk/clients/apigatewaymanagementapi'); 
+
+const CHATCONNECTION_TABLE = 'chatIdTable';
+
+const successfullResponse = {
+  statusCode: 200,
+  body: 'everything is alright WKIMDEV~'
+};
+
+
+module.exports.connectionManagement = (event, context, callback) => {
+  console.log(event);
+
+  if (event.requestContext.eventType === 'CONNECT') {
+     // Handle connection
+     addConnection(event.requestContext.connectionId)
+     .then(() => {
+       callback(null, successfullResponse);
+     })
+     .catch(err => {
+       console.log(err);
+       callback(null, JSON.stringify(err));
+     });
+  } else if (event.requestContext.eventType === 'DISCONNECT') {
+      // Handle disconnection
+      deleteConnection(event.requestContext.connectionId)
+      .then(() => {
+        callback(null, successfullResponse);
+      })
+      .catch(err => {
+        console.log(err);
+        callback(null, {
+          statusCode: 500,
+          body: 'Failed to connect: ' + JSON.stringify(err)
+        });
+      });
+  } 
+};
+
+// THIS ONE DOESNT DO ANYHTING
+module.exports.defaultHandler = (event, context, callback) => {
+  console.log('defaultHandler was called');
+  console.log(event);
+
+  callback(null, {
     statusCode: 200,
-    body: JSON.stringify(
-      {
-        message: 'Go Serverless v1.0! WKIMDEV~ Your function executed successfully!',
-        input: event,
-      },
-      null,
-      2
-    ),
+    body: 'defaultHandler'
+  });
+};
+
+// message를 보내면 lambda에 트리거가 걸린다. 
+module.exports.sendMessageHandler = (event, context, callback) => {
+  sendMessageToAllConnected(event).then(() => {
+    callback(null, successfullResponse)
+  }).catch (err => {
+    callback(null, JSON.stringify(err));
+  });
+}
+
+// 내 어플에 접근한 모든 사용자에게 메세지를 보냄
+const sendMessageToAllConnected = (event) => {
+  return getConnectionIds().then(connectionData => {
+    return connectionData.Items.map(connectionId => {
+      return send(event, connectionId.connectionId);
+    });
+  });
+}
+
+const send = (event, connectionId) => {
+  const body = JSON.parse(event.body);
+  const postData = body.data;  
+
+  const endpoint = event.requestContext.domainName + "/" + event.requestContext.stage;
+  const apigwManagementApi = new AWS.ApiGatewayManagementApi({
+    apiVersion: "2019-10-24",
+    endpoint: endpoint
+  });
+
+  const params = {
+    ConnectionId: connectionId,
+    Data: postData
+  };
+  // repeat every resource connected
+  return apigwManagementApi.postToConnection(params).promise();
+};
+
+// 웹소켓 커넥션이 연결되어있을때 connectionId를 add
+const addConnection = connectionId => {
+  const params = {
+    TableName: CHATCONNECTION_TABLE,
+    Item: {
+      connectionId: connectionId 
+    }
   };
 
-  // Use this code if you don't use the http event with the LAMBDA-PROXY integration
-  // return { message: 'Go Serverless v1.0! Your function executed successfully!', event };
+  return dynamo.put(params).promise();
+};
+
+// 웹소켓 커넥션이 끊겼을때 connectionId를 삭제
+const deleteConnection = connectionId => {
+  const params = {
+    TableName: CHATCONNECTION_TABLE,
+    Key: {
+      connectionId: connectionId 
+    }
+  };
+
+  return dynamo.delete(params).promise();
 };
